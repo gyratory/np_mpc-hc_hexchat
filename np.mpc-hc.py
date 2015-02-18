9003c37313f4da74c33ecedc4d3f6d1b5533ee72
gyratory <gyratory@users.noreply.github.com>
2015-02-17 18:45:24 -0800

    Initial Commit

---
+++ np.mpc-hc.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+#/**
+#* mpc.hc.np.py, snippet to display now-playing info for MPC-HC
+#* Released under the terms of MIT license
+#*
+#* https://github.com/gyratory/
+#*
+#* Copyright (C) 2013 MPC-HC Team
+#* Copyright (C) 2015 gyratory
+#*/
+
+
+__module_name__ = "MPC-HC NP snippet"
+__module_version__ = "0.3"
+__module_description__ = "Displays MPC-HC Player Info!"
+
+import hexchat
+import urllib.request
+import re
+
+###############################################################################
+# Setup
+
+MPC_HC_PORT = "13579"      # Default port
+MPC_HC_PAGE = "info.html"  # Page where "now playing" info is displayed
+
+###############################################################################
+
+MPC_HC_URL = "http://{0}:{1}/{2}".format("localhost", MPC_HC_PORT, MPC_HC_PAGE)
+
+MPC_HC_REGEXP = re.compile(r"\<p\ id\=\"mpchc_np\"\>(.*)\<\/p\>")
+
+
+def mpc_hc(caller, callee, helper):
+    data = urllib.request.urlopen(MPC_HC_URL).read()
+    mpc_hc_np = MPC_HC_REGEXP.findall(data.decode())[0].replace("&laquo;", "«")
+    mpc_hc_np = mpc_hc_np.replace("&raquo;", "»")
+    mpc_hc_np = mpc_hc_np.replace("&bull;", "•")
+    hexchat.command("say %s" % mpc_hc_np)
+    return hexchat.EAT_ALL
+
+hexchat.hook_command(
+    "np",
+    mpc_hc,
+    help="Use: /np :: Setup: Options -> Player -> Web Interface -> Listen on port"
+)
---
+++ readme.md
@@ -0,0 +1,15 @@
+#np.mpc-hc.py
+###Installation
+Copy this into the `%APPDATA%\HexChat\addons` folder.
+###Setup
+##MPC-HC
+Open MPC-HC, press `o` to open up the options.  Then under `Player->Web Interface` enable the option `Listen on Port`.
+##HexChat
+If you have not restarted HexChat, enter `/load np.mpc-hc.py`.
+#Troubleshooting
+If you get the message `Unknown file type np.mpc-hc.py`... see [here](https://hexchat.readthedocs.org/en/latest/faq.html#i-get-this-error-unknown-file-type-abc-yz-maybe-you-need-to-install-the-perl-or-python-plugin).
+###Usage
+`/np` send a message to your current channel about what you are playing on MPC-HC at the moment.
+###More
+Originally from [MPC-HC snippets](https://github.com/mpc-hc/snippets).
+Readme and filename copied from [derekxwu](https://github.com/derekxwu/hexchat.np.mpc-hc).
